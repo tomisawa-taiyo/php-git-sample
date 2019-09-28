dfa0deafc31da4849a8c195d1ca7693501101f9b
tomisawa-taiyo <taiyo1975@i.softbank.jp>
2019-09-28 06:35:53 +0000

    first commit

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -11,6 +11,8 @@ use App\History;
 
 use Carbon\Carbon;
 
+use Storage;
+
 
 class NewsController extends Controller
 {
@@ -30,8 +32,8 @@ class NewsController extends Controller
 
       // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
       if (isset($form['image'])) {
-        $path = $request->file('image')->store('public/image');
-        $news->image_path = basename($path);
+        $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+        $news->image_path = Storage::disk('s3')->url($path);
       } else {
           $news->image_path = null;
       }
@@ -95,8 +97,8 @@ class NewsController extends Controller
       $news_form = $request->all();
       
       if (isset($news_form['image'])) {
-        $path = $request->file('image')->store('public/image');
-        $news->image_path = basename($path);
+        $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+        $news->image_path = Storage::disk('s3')->url($path);
         unset($news_form['image']);
       } elseif (isset($request->remove)) {
         $news->image_path = null;
